5a18feaedd6837db893f49abc85d2ca1fd66a0d6
JLKellogg <jlkellogg18@outlook.com>
2020-02-08 18:10:04 -0700

    added highScores specific javascript file. added highScores html functionality except for carrying over scores.

---
+++ highScoresScript.js
@@ -0,0 +1,47 @@
+//high scores HTML
+var highScoreInput = document.querySelector("#highScoreText");
+var highScoreForm = document.querySelector("#highScoreForm");
+var highScoreList = document.querySelector("#highScoreList");
+var highScoreCountSpan = document.querySelector("#highScoreCount");
+var clearScores = document.querySelector("#clearScores");
+var goBack = document.querySelector("#goBackBtn");
+var highScores = [];
+
+renderHighScores();
+
+function renderHighScores() {
+  highScoreList.innerHTML = "";
+  highScoreCountSpan.textContent = highScores.length;
+
+  for (var i = 0; i < highScores.length; i++) {
+    var highScore = highScores[i];
+    var li = document.createElement("li");
+    li.textContent = highScore;
+    highScoreList.appendChild(li);
+  }
+}
+
+highScoreForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  var highScoreText = highScoreInput.value.trim();
+
+  if (highScoreText === "") {
+    //will return to top of function if input is blank
+    return;
+  }
+  highScores.push(highScoreText);
+  highScoreInput.value = "";
+
+  renderHighScores();
+});
+
+clearScores.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  var element = e.target;
+
+  if (element.matches("button")) {
+    highScores = [];
+  }
+  renderHighScores();
+});
