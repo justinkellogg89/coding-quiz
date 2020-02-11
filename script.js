3799dbb8a3e953cf8dc794e72d3cc2f71e97344b
Justin Kellogg <jlkellogg18@outlook.com>
2020-02-11 14:42:52 -0700

    fixed timer

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var $quiz = document.querySelector("#quiz");
 var $question = document.querySelector("#question");
 var $answer = document.querySelector("#answers");
 var questionIndex = 0;
-var $amountCorrect = document.querySelector("#amountCorrect");
-var $amountAsked = document.querySelector("#amountAsked");
+var $amountCorrect = document.querySelector("#amountCorrect").innerHTML;
+var $amountAsked = document.querySelector("#amountAsked").innerHTML;
 var $timer = document.querySelector("#timer");
 var questions = [
   {
@@ -13,7 +13,7 @@ var questions = [
     answers: [
       { text: "Strings", isCorrect: false },
       { text: "Booleans", isCorrect: false },
-      { text: "Alerts", isCorrect: true },
+      { text: "Alerts", isCorrect: true, },
       { text: "Numbers", isCorrect: false }
     ]
   },
@@ -58,44 +58,61 @@ var questions = [
   }
 ];
 // when user clicks 'begin quiz' button this will hide the greeting and display the quiz
-$beginBtn.addEventListener("click", function(e) {
+$beginBtn.addEventListener("click", function (e) {
   e.preventDefault();
   $greeting.style.display = "none";
   renderQuestion();
   $quiz.style.display = "block";
-  timer();
+  $amountAsked = 0;
+  $amountCorrect = 0;
+  $timer.style.display = "block";
+  counter = setInterval(timer, 1000);
 });
+
+$amountCorrect.innerHTML = "Amount Correct: " + $amountCorrect;
+$amountAsked.innerHTML = "Amount Asked: " + $amountAsked;
 
 // this runs the users click against the array of correct answers corresponding
 // with the questions in the array above.
-$answer.addEventListener("click", function(e) {
+$answer.addEventListener("click", function (e) {
   if (!e.target.matches("li")) return;
   var index = e.target.dataset.index;
   var correct = questions[questionIndex].answers[index].isCorrect;
   if (correct) {
     $amountCorrect++;
     $amountAsked++;
-    $amountCorrect.textContent = "Amount Correct: " + $amountCorrect;
-    $amountAsked.textContent = "Amount Asked: " + $amountAsked;
   } else {
     $amountAsked++;
-    $amountCorrect.textContent = "Amount Correct: " + $amountCorrect;
-    $amountAsked.textContent = "Amount Asked: " + $amountAsked;
   }
-
   questionIndex++;
   renderQuestion();
 });
+
 // this adds an effect to highlight the mouseover of the user blue and remove it when
 // they are no longer mousing over.
-$answer.addEventListener("mouseover", function(e) {
+$answer.addEventListener("mouseover", function (e) {
   if (!e.target.matches("li")) return;
   e.target.classList.add("active");
 });
-$answer.addEventListener("mouseout", function(e) {
+$answer.addEventListener("mouseout", function (e) {
   if (!e.target.matches("li")) return;
   e.target.classList.remove("active");
 });
+
+var count = 60;
+var counter;
+var running = false;
+do {
+} while (running != false)
+function timer() {
+  $timer.textContent = count;
+  count = count - 1;
+  if (count < 0) {
+    clearInterval(counter);
+    endGame();
+    return;
+  }
+}
 
 function renderQuestion() {
   if (questionIndex === questions.length) {
@@ -103,28 +120,20 @@ function renderQuestion() {
     return;
   }
 
-  var count = 60;
-  var counter = setInterval(timer, 1000);
-function timer() {
-    $timer.textContent = count;
-    count = count - 1;
-    if (count < 0) {
-      clearInterval(counter);
-      endGame();
-      return;
-    }
-  }
-  function endGame() {
-    $quiz.style.display = "none";
-  }
+
   var question = questions[questionIndex];
   $question.textContent = question.question;
   $answer.innerHTML = "";
-  question.answers.forEach(function(answer, i) {
+  question.answers.forEach(function (answer, i) {
     var li = document.createElement("li");
     li.textContent = answer.text;
     li.setAttribute("data-index", i);
     li.setAttribute("class", "list-group-item");
     $answer.append(li);
   });
+}
+
+function endGame() {
+  $quiz.style.display = "none";
+  clearInterval(counter);
 }
